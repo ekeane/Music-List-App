e613daeabeca52a6b32ee8ab4fa94fe831d17878
Eamonn Keane <ekeane2015@gmail.com>
2016-06-14 02:23:47 +0000

    New changes

---
+++ app/actions.rb
@@ -0,0 +1,34 @@
+# Homepage (Root path)
+get '/' do
+  @songs = Song.all
+  erb :index
+end
+
+get '/songs' do
+  @songs = Song.all
+  erb :'songs/index'
+end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
+
+post '/songs' do
+
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    content: params[:content]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
